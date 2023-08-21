61509ce46f3c508267f07eb53be42c94fc534eb2
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-08-21 18:31:59 -0400

    lab completed

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,13 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="", breed="Pug"):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        elif breed not in APPROVED_BREEDS:
+            print("Breed must be in the list of approved breeds.")
+        else:
+            self.name = name
+            self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,11 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="", job=""):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be a string between 1 and 25 characters.")
+        elif job not in APPROVED_JOBS:
+            print("Job must be in the list of approved jobs.")
+        else:
+            self.name = name
+            self.job = job
